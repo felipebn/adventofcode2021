79959ab08ca529cec52fb93d3b880dcda60d4039
felipebn <thrash.nation@gmail.com>
2021-12-03 01:04:42 +0000

    bootstrap elixir project

---
+++ adv21/lib/adv21.ex
@@ -0,0 +1,18 @@
+defmodule Adv21 do
+  @moduledoc """
+  Documentation for `Adv21`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adv21.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
