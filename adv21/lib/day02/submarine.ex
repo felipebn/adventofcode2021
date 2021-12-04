39d6da7fa8e1880d4a19aa1d2484d94a8ba5729c
felipebn <thrash.nation@gmail.com>
2021-12-04 03:20:47 +0000

    add placeholder for part2

--- adv21/lib/day02/submarine.ex
+++ adv21/lib/day02/submarine.ex
@@ -16,4 +16,19 @@ defmodule Adv21.Day02.Submarine do
     x*y
   end
 
+  def follow_course_with_aim(filename) do
+    {x, y} = File.stream!(filename)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(fn step -> String.split(step, " ") end)
+    |> Enum.map(fn [direction, value] -> {direction, String.to_integer(value)} end)
+    |> Enum.reduce({0, 0}, fn step, {xx,yy} ->
+      case step do
+        {"forward", count} -> {xx + count, yy}
+        {"down", count} -> {xx, yy + count}
+        {"up", count} -> {xx, yy - count}
+      end
+    end)
+
+    x*y
+  end
 end
