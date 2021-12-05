1d8390523e5d2119b77fe1096baf640f4110dfc9
felipebn <thrash.nation@gmail.com>
2021-12-05 00:02:40 +0000

    add day02 part2 implementation

--- adv21/lib/day02/submarine.ex
+++ adv21/lib/day02/submarine.ex
@@ -17,15 +17,15 @@ defmodule Adv21.Day02.Submarine do
   end
 
   def follow_course_with_aim(filename) do
-    {x, y} = File.stream!(filename)
+    {_, x, y} = File.stream!(filename)
     |> Enum.map(&String.trim/1)
     |> Enum.map(fn step -> String.split(step, " ") end)
     |> Enum.map(fn [direction, value] -> {direction, String.to_integer(value)} end)
-    |> Enum.reduce({0, 0}, fn step, {xx,yy} ->
+    |> Enum.reduce({0, 0, 0}, fn step, {aim, xx, yy} ->
       case step do
-        {"forward", count} -> {xx + count, yy}
-        {"down", count} -> {xx, yy + count}
-        {"up", count} -> {xx, yy - count}
+        {"forward", value} -> {aim, xx + value, yy + (aim * value)}
+        {"down", value} -> {aim + value, xx, yy}
+        {"up", value} -> {aim - value, xx, yy}
       end
     end)
 
--- adv21/test/day02/submarine_tests.exs
+++ adv21/test/day02/submarine_tests.exs
@@ -13,4 +13,15 @@ defmodule SubmarineTest do
     course_position = Submarine.follow_course("lib/day02/input.txt")
     IO.puts("Course position in example file #{course_position} ")
   end
+
+  test "course position with aim for example file returns 900" do
+    course_position = Submarine.follow_course_with_aim("test/day02/example.txt")
+    IO.puts("Course position in example file (with aim) #{course_position} ")
+    assert course_position == 900
+  end
+
+  test "course position with aim for input file" do
+    course_position = Submarine.follow_course_with_aim("lib/day02/input.txt")
+    IO.puts("Course position in example file (with aim) #{course_position} ")
+  end
 end
